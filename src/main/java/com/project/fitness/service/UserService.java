b83dfe16529d8c520f9a698f4df3b70d4d38844b
harshalsaonerkar <harshalsaonerkar702@gmail.com>
2026-01-22 13:44:01 +0530

    Added JWT authentication

--- src/main/java/com/project/fitness/service/UserService.java
+++ src/main/java/com/project/fitness/service/UserService.java
@@ -1,11 +1,13 @@
 package com.project.fitness.service;
-
-
+import com.project.fitness.dto.LoginRequest;
 import com.project.fitness.dto.RegisterRequest;
 import com.project.fitness.dto.UserResponse;
 import com.project.fitness.model.User;
+import com.project.fitness.model.UserRole;
 import com.project.fitness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +15,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse register(RegisterRequest request) {
+        UserRole role = request.getRole() != null ? request.getRole() : UserRole.USER;
+
         User user = User.builder()
                 .email(request.getEmail())
                 .lastName(request.getLastName())
                 .firstName(request.getFirstName())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(role)
                 .build();
 //        User user = new User(
 //                null,
@@ -39,7 +45,7 @@ public class UserService {
         return mapToResponse(savedUser);
     }
 
-    private UserResponse mapToResponse(User savedUser) {
+    public UserResponse mapToResponse(User savedUser) {
         UserResponse response = new UserResponse();
         response.setId(savedUser.getId());
         response.setEmail(savedUser.getEmail());
@@ -49,5 +55,16 @@ public class UserService {
         response.setCreatedAt(savedUser.getCreatedAt());
         response.setUpdatedAt(savedUser.getUpdatedAt());
         return response;
+    }
+
+    public User authenticate(LoginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+        if(user == null)
+            throw new RuntimeException("Invaild Credentials");
+
+        if(!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())){
+            throw new RuntimeException("Invalid Credentials");
+        }
+        return user;
     }
 }
